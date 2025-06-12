abe84afe5ceb3d6a3b223a17f80f2b2d90215d56
Marlena <s19345@pjwstk.edu.pl>
2025-06-12 19:08:25 +0200

    file watch

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
+import json
+import sys
+import logging
+from pathlib import Path
 
-def print_hi(name):
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler, FileSystemEventHandler
 
-    print(f'Hi, {name}')
+def load_config(file):
+    with open(file) as f:
+        return json.load(f)
 
+class FileHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        filepath = Path(config["WATCHED_DIR"])
+        if filepath.suffix in ['.csv', '.xlsx']:
+            print(f"Znaleziono plik: {filepath.name}")
 
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
+if __name__ == "__main__":
+    config = load_config("config.json")
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    # observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    print(config["WATCHED_DIR"])
+    try:
+        while observer.is_alive():
+            observer.join(1)
+    finally:
+        observer.stop()
+        observer.join()
